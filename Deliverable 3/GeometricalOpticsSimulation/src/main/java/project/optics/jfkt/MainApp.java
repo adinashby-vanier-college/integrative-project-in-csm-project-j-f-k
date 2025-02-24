7a3671a99a36e0ef7b326edb08a6e7c6c34620c7
Kevin-Yichen Li <2358068@edu.vaniercollege.qc.ca>
2025-02-24 10:42:00 -0500

    Project initialization

---
+++ Deliverable 3/GeometricalOpticsSimulation/src/main/java/project/optics/jfkt/MainApp.java
@@ -0,0 +1,32 @@
+package edu.vanier.template;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * This is a JavaFX project template to be used for creating GUI applications.
+ * JavaFX 20.0.2 is already linked to this project in the build.gradle file.
+ * @link: https://openjfx.io/javadoc/22/
+ * @see: Build Scripts/build.gradle
+ * @author Frostybee.
+ */
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Button btnClickMe = new Button("Click me!");
+        root.getChildren().add(btnClickMe);
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
