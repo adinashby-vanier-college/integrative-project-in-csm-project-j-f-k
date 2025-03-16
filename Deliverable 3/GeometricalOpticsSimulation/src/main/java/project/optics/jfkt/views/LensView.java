af28e8c1d1561b5de690a783c724c11d00b20637
Tristan <tristan.giannopoulos@gmail.com>
2025-03-16 10:11:44 -0400

    lens view clearing of unused imports

--- Deliverable 3/GeometricalOpticsSimulation/src/main/java/project/optics/jfkt/views/LensView.java
+++ Deliverable 3/GeometricalOpticsSimulation/src/main/java/project/optics/jfkt/views/LensView.java
@@ -1,12 +1,5 @@
 package project.optics.jfkt.views;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-
-import java.io.IOException;
-
 public class LensView extends BaseView {
 
 
